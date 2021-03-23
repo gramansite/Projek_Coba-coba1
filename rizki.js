3f16c2c1fb634b3f1ccd46fcb33c3048aa01d3bd
gramansite <gramansite@gmail.com>
2021-03-23 20:17:04 +0700

    update

--- rizki.js
+++ rizki.js
@@ -1,10 +1,28 @@
+// JS untuk Menu Navbar Scroll Change Color
 $(window).scroll(function () {
   var y = $(window).scrollTop();
   if (y >= 100) {
-    $("nav .container").css("background-color", "green");
+    $("header").css("background-color", "#0099ff");
   } else if (y >= 0) {
-    $("nav .container").css("background-color", "#26d0c9");
+    $("header").css("background-color", "rgba(34, 49, 52, 0.9)");
   } else {
-    $("nav .container").css("background-color", "#26d0c9");
+    $("header").css("background-color", "#26d0c9");
   }
+});
+
+// JS untuk Menu Open - Focus
+$(function () {
+  var pull = $("#pull"); // Variabel tombol navigasi (akan muncul hanya pada perangkat mobile)
+  menu = $("nav ul"); // Variabel menu
+
+  $(pull).on("click", function (e) {
+    e.preventDefault();
+    menu.slideToggle();
+  });
+  $(window).resize(function () {
+    var w = $(window).width();
+    if (w > 600 && menu.is(":hidden")) {
+      menu.removeAttr("style");
+    }
+  });
 });
