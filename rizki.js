797f442ef8503c4c79e9cb6c29aefbdafc6c75a8
gramansite <gramansite@gmail.com>
2021-05-26 20:41:30 +0700

    menambahkan

--- rizki.js
+++ rizki.js
@@ -2,27 +2,27 @@
 $(window).scroll(function () {
   var y = $(window).scrollTop();
   if (y >= 100) {
-    $("header").css("background-color", "#0099ff");
+    $('header').css('background-color', '#0099ff');
   } else if (y >= 0) {
-    $("header").css("background-color", "rgba(34, 49, 52, 0.9)");
+    $('header').css('background-color', 'rgba(34, 49, 52, 0.9)');
   } else {
-    $("header").css("background-color", "#26d0c9");
+    $('header').css('background-color', '#26d0c9');
   }
 });
 
 // JS untuk Menu Open - Focus
 $(function () {
-  var pull = $("#pull"); // Variabel tombol navigasi (akan muncul hanya pada perangkat mobile)
-  menu = $("nav ul"); // Variabel menu
+  var pull = $('#pull'); // Variabel tombol navigasi (akan muncul hanya pada perangkat mobile)
+  menu = $('nav ul'); // Variabel menu
 
-  $(pull).on("click", function (e) {
+  $(pull).on('click', function (e) {
     e.preventDefault();
     menu.slideToggle();
   });
   $(window).resize(function () {
     var w = $(window).width();
-    if (w > 600 && menu.is(":hidden")) {
-      menu.removeAttr("style");
+    if (w > 600 && menu.is(':hidden')) {
+      menu.removeAttr('style');
     }
   });
 });
